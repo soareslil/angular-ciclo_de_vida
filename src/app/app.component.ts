957f708ba35e1417724855a85d35652f2c7d0130
soareslil <lilianectd@gmail.com>
2023-03-30 14:29:40 -0300

    oninit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+import { Item } from './interfaces/iItem';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app-lista-de-compras';
 
-  constructor() { }
+  listaDeCompra! : Array<Item>
+
+  constructor(private listaService: ListaDeCompraService) { }
+
+  ngOnInit(): void {
+    this.listaDeCompra = this.listaService.getListaDeCompra();
+    console.log(this.listaDeCompra);
+
+  }
+
 }
